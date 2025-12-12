b9104c11a6803188813f5e42cbaf58ed9b593c27
bunny123373 <korrasanhok@gmail.com>
2025-12-12 11:11:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const path = require('path');
 const jwt = require('jsonwebtoken');
 
-const Video = require('./models/Video');
+const Video = require('./Video');
 
 const app = express();
 app.use(cors());
